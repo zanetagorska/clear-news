f565422ad6590e4b1518006489a2cab58dd475bd
Żaneta Górska <zaneta.gorska1@gmail.com>
2017-04-10 08:39:36 +0200

    Header swiper slider - wstępny wygląd

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,1 +1,17 @@
-    var swiper = new Swiper('.swiper-container');
+var swiper = new Swiper('.swiper-container', {
+  nextButton: '.swiper-button-next',
+  prevButton: '.swiper-button-prev',
+  spaceBetween: 30,
+  parallax: true,
+  speed: 600
+});
+
+$(function() {
+  var slides = $('.swiper-slide');
+  for (i=0; i<slides.length; i++) {
+    imageUrl = 'assets/img/slider/'+(i+1)+'.jpg';
+    var slide = slides[i];
+    var $slide = $(slide);
+    $slide.css('background-image', 'url(' + imageUrl + ')');
+  }
+});
