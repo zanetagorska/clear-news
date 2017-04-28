bbdbefe5c467b6e832d99c31a8939f7a639dd3d4
Żaneta Górska <zaneta.gorska1@gmail.com>
2017-04-28 11:30:30 +0200

    navbar - struktura i rwd

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -15,9 +15,39 @@ $(function() {
     var $slide = $(slide);
     $slide.css('background-image', 'url(' + imageUrl + ')');
   }
-});
 
-// Menu
-      $(document).ready(function() {
-        $('#video-gallery').lightGallery(); 
-    });
+// Colapsed menu
+  $('.fa-bars').click(function(e){
+    e.preventDefault();
+    $('.level1-ul').toggle();
+  });
+
+// odpowiedni display menu dla różnych ekranów
+  $(window).resize(function(){
+    if($(window).width()>762){
+      $('.level1-ul').css('display', 'flex');
+    }else{
+      $('.level1-ul').css('display', 'block');
+    }
+    if($(window).width()<450){
+      $('.level2-ul').css('display', 'none');
+    }else{
+      $('level2-ul').css('display', 'flex');
+    }
+  });
+
+// Otwieranie drugiego poziomu listy
+  $('#entertainment').click(function(e){
+    if($(window).width()>449){
+      e.preventDefault();
+      $('.level2-ul').toggle();
+    };
+  });
+
+
+});
+// Menu level 3 video
+  $(document).ready(function() {
+    $('#video-gallery').lightGallery(); 
+  });
+
